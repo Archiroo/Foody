f14d57d88c0b1b41a5b117e2efd96f06d20ca008
Archiroo <tailieuit.nb@gmail.com>
2022-09-29 23:30:57 +0700

    Lên giao diện trang Customer/Home

---
+++ customer/about/index.php
@@ -0,0 +1,9 @@
+<?php
+    // Header start
+    include('../header.php');
+    // Header end
+
+    // Footer start
+    include('../footer.php');
+    // Footer end
+?>
---
+++ customer/home/index.php
@@ -0,0 +1,44 @@
+<?php 
+    // Header start
+    include('header.php');
+    // Header end
+    
+    // Banner start
+    include('banner.php');
+    // Banner end
+
+    // About start
+    include('about.php');
+    // About end
+
+    // About start
+    include('feature.php');
+    // About end
+
+    // Product start
+    include('product.php');
+    // Product end
+
+    // Testimonial start
+    include('customer_review.php');
+    // Testimonial end
+
+    // Footer start
+    include('footer.php');
+    // Footer end
+?>
+
+
+    
+
+    
+
+
+    
+
+
+    
+
+    
+
+
