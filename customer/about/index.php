4ffe9187419af7a304c59b83e746cb21d34ff9c1
Archiroo <tailieuit.nb@gmail.com>
2022-10-03 16:16:24 +0700

    Màn hình about

---
+++ customer/about/index.php
@@ -0,0 +1,28 @@
+<?php 
+    // Header start
+    include('header.php');
+    // Header end
+
+    // Navbar start
+    include('navbar.php');
+    // Navbar end
+
+    // Banner start
+    include('banner.php');
+    // Banner end
+
+    // About start
+    include('about.php');
+    // About end
+
+    // About start
+    include('feature.php');
+    // About end
+
+    // Footer start
+    include('footer.php');
+    // Footer end
+
+
+
+?>
