d0e9455c69a81ffbfbfc9d0bbb7a11cbf811fd5a
Archiroo <tailieuit.nb@gmail.com>
2022-10-03 15:58:56 +0700

    Trang home

---
+++ customer/index.php
@@ -0,0 +1,33 @@
+<?php 
+    // Header start
+    include('header.php');
+    // Header end
+
+    // Navbar start
+    include('home/navbar.php');
+    // Navbar end
+
+    // Banner start
+    include('home/banner.php');
+    // Banner end
+
+    // Banner start
+    include('home/about.php');
+    // Banner end
+
+    // Banner start
+    include('home/product.php');
+    // Banner end
+
+    // Banner start
+    include('home/customer_review.php');
+    // Banner end
+
+    // Banner start
+    include('home/blog.php');
+    // Banner end
+
+    // Footer start
+    include('footer.php');
+    // Footer end
+?>
