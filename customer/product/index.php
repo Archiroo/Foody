3a1ee9e274dd28c8f1eea76497f41fc853d0fd7d
Archiroo <tailieuit.nb@gmail.com>
2022-10-03 17:01:58 +0700

    Thêm màn hình product

---
+++ customer/product/index.php
@@ -0,0 +1,26 @@
+<?php
+    // Header start
+    include('../about/header.php');
+    // Header end
+    
+    // Navbar start
+    include('../about/navbar.php');
+    // Navbar end
+
+    // Banner start
+    include('../about/banner.php');
+    // Banner end
+
+    // Product start
+    include('product.php');
+    // Product end
+
+    // Product start
+    include('customer_review.php');
+    // Product end
+
+    // Product start
+    include('../about/footer.php');
+    // Product end
+
+?>
