0f1b5b70ea6ca551367cd9e48ef960cc2de71166
Archiroo <90332385+Archiroo@users.noreply.github.com>
2022-10-08 11:19:44 +0700

    Demo popup

--- popup/script.js
+++ popup/script.js
@@ -1,14 +1,14 @@
-var openModal = document.querySelector('.open_model');
-var modal = document.querySelector('.model');
-var iconClose = document.querySelector('.model_header i');
-var closeModal = document.querySelector('.model_footer button');
+var openModal = document.querySelector('.open_modal');
+var modal = document.querySelector('.modal');
+var iconClose = document.querySelector('.modal_header i');
+// var closeModal = document.querySelector('.model_footer button');
 
 function toggeModal() {
     modal.classList.toggle('hide')
 }
 openModal.addEventListener('click', toggeModal);
 iconClose.addEventListener('click', toggeModal);
-closeModal.addEventListener('click', toggeModal);
+// closeModal.addEventListener('click', toggeModal);
 modal.addEventListener('click', function(e) {
     if(e.target == e.currentTarget) {
         toggeModal()    
