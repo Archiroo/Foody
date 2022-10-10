e3c8811a6d117897461a987b8a329f84f3c7c99d
diennguyen2611 <90564453+diennguyen2611@users.noreply.github.com>
2022-10-10 10:24:15 +0700

    abc

--- admin/footer.php
+++ admin/footer.php
@@ -1,8 +1,9 @@
 </main>
-    <!-- JQUERY -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-    <!-- SCRIPT -->
-    <script src="js/script.js"></script>
+<!-- JQUERY -->
+
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
 </body>
 
 </html>
--- admin/header.php
+++ admin/header.php
@@ -1,10 +1,10 @@
 <?php
-    include('connect.php');
-    if(!isset($_SESSION['id_admin']))
-    {
-        header("Location:../index.php");
-    }
+include('connect.php');
+if (!isset($_SESSION['id_admin'])) {
+    header("Location:../index.php");
+}
 ?>
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -13,16 +13,18 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
     <!-- <link rel="stylesheet" href="../customer/css/bootstrap.min.css"> -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css">
     <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="css/update.css">
     <title>Foody Website</title>
 </head>
+
 <body>
     <input type="checkbox" id="sidebar-toggle">
     <div class="sidebar">
         <div class="header-sidebar">
             <h3 class="brand">
-            <span class="fa-solid fa-seedling"></span>
+                <span class="fa-solid fa-seedling"></span>
                 <span>Foody</span>
             </h3>
             <label for="sidebar-toggle" class="ti-menu-alt"></label>
@@ -58,6 +60,35 @@
                     </a>
                 </li>
                 <li>
+                    <div>
+                        <div id="headingOne">
+                            <h5 class="mb-0">
+                                <a data-toggle="collapse" data-target="#collapseOne" aria-expanded="true"
+                                    aria-controls="collapseOne">
+                                    <span class="fa-solid fa-spa"></span>
+                                    <span>Sản phẩm<i class="fa-solid fa-sort-down flex-right" style="margin-left: 50px;"></i></span>
+                                    
+                                </a>
+                            </h5>
+                        </div>
+                        <ul id="collapseOne" class="collapse" aria-labelledby="headingOne"
+                            data-parent="#accordionExample">
+                            <li>
+                                <a href="typeProduct.php">
+                                    <i class="fa-regular fa-circle-stop"></i>
+                                    <span>Quản lý loại sản phẩm</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="typeProduct.php">
+                                    <i class="fa-regular fa-circle-stop"></i>
+                                    <span>Quản lý sản phẩm</span>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </li>
+                <li>
                     <a href="#">
                         <span class="fa-solid fa-house-chimney"></span>
                         <span>Hóa đơn</span>
@@ -83,24 +114,24 @@
         <header>
             <div class="search-wrapper">
                 <!-- <span class="ti-search"></span>
-                <input type="text" placeholder="Tìm kiếm"> -->
+                        <input type="text" placeholder="Tìm kiếm"> -->
             </div>
             <div class="social-icons">
                 <div>
                     <?php
-                        $id_admin = $_SESSION['id_admin'];
-                        $sql = "SELECT * FROM tbl_user WHERE id = '$id_admin'";
-                        $result = mysqli_query($connect,$sql);
-                        if($result == TRUE){
-                            $count = mysqli_num_rows($result);
-                            if($count>0){
-                                while($row = mysqli_fetch_assoc($result)){
-                                    $image_user = $row['image'];
-                                }
-                            }
-                        }
-                    ?>
+$id_admin = $_SESSION['id_admin'];
+$sql = "SELECT * FROM tbl_user WHERE id = '$id_admin'";
+$result = mysqli_query($connect, $sql);
+if ($result == TRUE) {
+    $count = mysqli_num_rows($result);
+    if ($count > 0) {
+        while ($row = mysqli_fetch_assoc($result)) {
+            $image_user = $row['image'];
+        }
+    }
+}
+?>
                     <img src="../image/<?php echo $image_user; ?>" alt="">
                 </div>
             </div>
-        </header> 
+        </header>
